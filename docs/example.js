e69ef74425d7f23b9db3fd84a4ec120e127bb36a
Iakov Salikov <salikovpro@gmail.com>
2020-10-27 17:43:36 +0200

    feat: Added render factory

--- docs/example.js
+++ docs/example.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hqCropper = HqCropper((image) => {
         document.querySelector('#image-preview').setAttribute('src', image)
-    })
+    }, { })
 
     document.querySelector('#change-image-button')
         .addEventListener('click', hqCropper.open, false)
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 const config = {
     input: 'lib/index.js',
     output: {
         file: 'dist/main.min.js',
         format: 'iife',
+        plugins: [
+            terser(),
+        ],
     },
-    plugins: [babel({ babelHelpers: 'bundled' })],
+    plugins: [
+        babel({ babelHelpers: 'bundled' }),
+    ],
 }
 
 export default config
